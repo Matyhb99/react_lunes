8784fd50e32a310093fe8719e16ec34102215454
unknown <sistemas@VRE_CAP21322.inacap.cl>
2026-05-04 17:45:43 -0400

    cambios en el titulo

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>aguante el bulla</h1>
+          <h1>Sale su asado</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
